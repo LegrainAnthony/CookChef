bfc8f44ed11d0ed009f40e21c3db9191e1eca74c
Anthony Legrain <legrain.anthony51@gmail.com>
2022-12-13 19:33:46 +0100

    Search system done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import styles from "./Header.module.scss"
 import cookchef from '../assets/images/cookchef.png'
+import HeaderMenu from "./headerMenu";
 
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
         <header className={`${styles.header} d-flex flex-row align-items-center`}>
-            <i class="fa-solid fa-bars mr-5"></i>
             <div className="flex-fill">
             <img  src={cookchef} alt="logo cookchef" />
             </div>
-            <ul>
-                <button className="mr-5 btn btn-reverse-primary"><span><i class="fa-solid fa-basket-shopping mr-5"></i></span> Panier</button>
+            <ul className={ styles.headerList}>
+                <button className="mr-5 btn btn-reverse-primary"><span><i class="fa-solid fa-heart mr-5"></i></span>Wishlist</button>
                 <button className="mr-5 btn btn-primary">Connexion</button>
             </ul>
+            <i  onClick={() => setShowMenu(true)} class={`fa-solid fa-bars ${styles.headerXs}`}></i>
+            {showMenu && 
+            <>
+            <div onClick={() => setShowMenu(false)} className="calc" ></div>
+            <HeaderMenu />
+            </>
+            }
         </header>
     );
 }
