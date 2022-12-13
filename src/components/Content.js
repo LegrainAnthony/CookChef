ea3e392ac4e3b7f387f848680d3b3c8d598506a6
Anthony Legrain <legrain.anthony51@gmail.com>
2022-12-13 14:50:32 +0100

    Like system for recipes

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,21 @@
+import styles from "./Content.module.scss"
+import Recipe from "./Recipe";
+import { data } from "../data/recipes"
+
 function Content() {
+
+const recipes = data;
+
     return (
-        <div className="flex-fill b3">
-            Content
+        <div className="flex-fill container p-20">
+            <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+            <div className={`card p-20 ${styles.contentCard}`}>
+                <div className={ styles.grid }>
+                    {recipes.map((recipe) => {
+                        return <Recipe  title={recipe.title} image={recipe.image}/>
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/data/recipes.js
@@ -0,0 +1,35 @@
+export const data = [
+
+    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    },    {
+        title: "Saumon et Asperges",
+        image: "assets/images/saumonasperges.jpg",
+    }
+
+]
