298512b628b2b302fdfe2bb2ca9ba3b964dfe795
Anthony Legrain <legrain.anthony51@gmail.com>
2022-12-14 21:07:47 +0100

    commit for pull to mac

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss'
 function Footer() {
     return (
         <footer className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}>
-            <p>Copyright © 2022 Anthony Legrain </p>
+            <p>No Copyright © 2022 Anthony Legrain </p>
         </footer>
     );
 }
